1907246006577f35dcd4639313db45e7e2bf514b
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-08-22 10:05:33 -0400

    update to task 4

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import satData from './satData.jsx';
+
+console.log(satData);
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {orbitTypes.map((type, index) => {
+        return (
+          <button onClick={() => filterByType(type)} key={index}>
+            {type} Orbit
+          </button>
+        );
+      })}
+
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
